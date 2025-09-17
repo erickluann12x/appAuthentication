d6184f8a72c836ce047291e461cb9824231246bc
erickluann12x <erickluan12x@gmail.com>
2025-09-16 22:31:08 -0300

    UserModel config

--- src/main/java/br/appAuth/appAuthentication/model/User.java
+++ src/main/java/br/appAuth/appAuthentication/model/User.java
@@ -1,5 +1,6 @@
 package br.appAuth.appAuthentication.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,16 +10,49 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 @Entity
-@Table (name = "users")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
-
+    @Column (unique = true,nullable = false)
     private String username;
 
+    @Column (nullable = false)
     private String email;
 
-    private int password;
+    @Column (nullable = false)
+    private String password;
+    public User () {}
+
+    public User(String username,String email,String password){
+    this.username =username;
+    this.email =email;
+    this.password =password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
